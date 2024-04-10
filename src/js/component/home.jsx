5557380c19fefbeb81ae495c0ba94492408ee84e
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-04-10 19:33:16 +0000

    check nuevo usuario

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,67 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+
+
+
+const Home = () => { 
+	const [inputTarea,setInputTarea] = useState ("")
+	const [listaTareas, setListaTareas] = useState ([]);
+
+	function escribirInput (event) {
+		setInputTarea(event.target.value);
+	}
+	
+	const pulsarEnter = (e) => {
+		if (e.key === "Enter" && inputTarea.trim()   !== "") {
+			crearTarea(inputTarea);
+		}
+	};
+
+	const crearTarea = (inputTarea) => {
+		const myHeaders = new Headers ();
+		myHeaders.append ("Content-Type", "application/json");
+
+		const raw = JSON.stringify ({
+			"label":inputTarea,
+			"is_done": false 
+		});
+
+		const requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch("https://playground.4geeks.com/todo/users/annamartinez-02", requestOptions)
+			.then((response) => response.json())
+			.then((result) => console.log(result))
+			.catch((error) => console.error(error));
+
+	}
+
+
+	useEffect (( )=>{
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		  };
+		  
+		  fetch("https://playground.4geeks.com/todo/users/annamartinez02", requestOptions)
+			.then((response) => response.json())
+			.then((result) => console.log(result))
+			.catch((error) => console.error(error));
+	}, [])
+	
+
+		
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<p> Helooo</p>
 	);
 };
 
