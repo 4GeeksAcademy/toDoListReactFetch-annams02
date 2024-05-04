8310a1e50276440de94b4d3a6d2977eac68d9f58
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-05-04 12:46:49 +0000

    faltan estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,25 +8,25 @@ import React, {useEffect, useState} from "react";
 
 
 const Home = () => { 
-	const [inputTarea,setInputTarea] = useState ("")
-	const [listaTareas, setListaTareas] = useState ([]);
+	const [inputTarea,setInputTarea] = useState ("")  //iniciamos con string vacío inputTarea
+	const [listaTareas, setListaTareas] = useState ([]); // iniciamos con array vacío listaTareas
 
-	function escribirInput (event) {
-		setInputTarea(event.target.value);
+	const escribirInput = (event) => {   // definimos función clásica: const NombreFunción = (parametros) => {} // el event es un parametro 
+		setInputTarea(event.target.value); // aquí accderás al valor del input que escriba el usuario y el setInputTarea actualizará la var InputTarea
 	}
 	
 	const pulsarEnter = (e) => {
-		if (e.key === "Enter" && inputTarea.trim()   !== "") {
-			crearTarea(inputTarea);
+		if (e.key === "Enter" && inputTarea.trim()   !== "") { // controlar el enter, .trim es para quitar espacios.  así no se me guardará nueva tarea con espacios vacíos solo
+			crearTarea(inputTarea); 
 		}
 	};
 
-	const crearTarea = (inputTarea) => {
-		const myHeaders = new Headers ();
+	const crearTarea = (taskName) => {
+		const myHeaders = new Headers ();    // de esta  l25, a la l43, lo saco de postman (code)
 		myHeaders.append ("Content-Type", "application/json");
 
 		const raw = JSON.stringify ({
-			"label":inputTarea,
+			"label":taskName,                  // esto lo saco de la documentación de la API (/todo/username) la label le doy valor taskName pq es el nombre del parametro de la l.24, y la llamo en crearTarea (l20). 
 			"is_done": false 
 		});
 
@@ -37,8 +37,14 @@ const Home = () => {
 			redirect: "follow"
 		};
 
-		fetch("https://playground.4geeks.com/todo/users/annamartinez-02", requestOptions)
-			.then((response) => response.json())
+		fetch("https://playground.4geeks.com/todo/todos/annams02", requestOptions)
+			.then(async (response) => {
+				const jsonResponse = await response.json();
+				setListaTareas([...listaTareas, {
+					"label":taskName,                  // esto lo saco de la documentación de la API (/todo/username) la label le doy valor taskName pq es el nombre del parametro de la l.24, y la llamo en crearTarea (l20). 
+					"is_done": false 
+				}]);
+			})
 			.then((result) => console.log(result))
 			.catch((error) => console.error(error));
 
@@ -51,8 +57,11 @@ const Home = () => {
 			redirect: "follow"
 		  };
 		  
-		  fetch("https://playground.4geeks.com/todo/users/annamartinez02", requestOptions)
-			.then((response) => response.json())
+		  fetch("https://playground.4geeks.com/todo/users/annams02", requestOptions)
+			.then(async (response) => {
+				const jsonResponse = await response.json();
+				setListaTareas(jsonResponse.todos);
+			})
 			.then((result) => console.log(result))
 			.catch((error) => console.error(error));
 	}, [])
@@ -61,7 +70,19 @@ const Home = () => {
 		
 
 	return (
-		<p> Helooo</p>
+		<div className="container">
+			<div className="text-center txt-light my-4">
+				<h1><strong>TO DO LIST</strong></h1>
+				<ul>
+					<li><input className= "form-control m-auto" type="text" onChange={escribirInput} onKeyDown={pulsarEnter} value={inputTarea} placeholder="What do you need in your to do list?" ></input> </li>
+					{listaTareas.map((tarea, index) => <li key={index}>{tarea.label} <button onClick={() => deleteItems (index)}><i className="fa-solid fa-trash"></i></button></li>)}
+			
+				</ul>	
+			</div>
+
+				<div className="text-center text-bold my-4 total"> {listaTareas.length} tasks </ div>
+
+		</div>
 	);
 };
 
